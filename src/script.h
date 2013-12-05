29e058695165a6396f214963ba9663c5f8c98253
ClaireDuSoleil <c52082000-2@yahoo.com>
2013-12-04 20:17:44 -0800

    more project setup stuff and also moved qobjects out of cpp into .h to ease the creation of the MSVC QT projects.

--- src/script.h
+++ src/script.h
@@ -284,9 +284,9 @@ public:
     CScript() { }
     CScript(const CScript& b) : std::vector<unsigned char>(b.begin(), b.end()) { }
     CScript(const_iterator pbegin, const_iterator pend) : std::vector<unsigned char>(pbegin, pend) { }
-#ifndef _MSC_VER
+//#ifndef __MSC_VER
     CScript(const unsigned char* pbegin, const unsigned char* pend) : std::vector<unsigned char>(pbegin, pend) { }
-#endif
+//#endif
 
     CScript& operator+=(const CScript& b)
     {
